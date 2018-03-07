b188aeb5ed1b1113881c0741f979857c4e51dec3
Zach Nusbaum <zachdnusbaum@gmail.com>
2018-03-06 23:44:36 -0800

    Add status update mailer

--- app/mailers/support/tickets_mailer.rb
+++ app/mailers/support/tickets_mailer.rb
@@ -3,9 +3,17 @@ class Support::TicketsMailer < ApplicationMailer
   def ticket_created(ticket_id)
     @ticket = Support::Ticket.find(ticket_id)
 
-    mail to: @ticket.user.email,
-      from: 'staff-support@pincanna.org',
+    mail to: "#{@ticket.user.name} <#{@ticket.user.email}>",
+      from: 'PincannaRx Support <staff-support@pincanna.org>',
       subject: "[#{@ticket.ticket_number}] New Support Ticket",
-      cc: Rails.application.credentials.admin_email
+      cc: "System Administrator <#{Rails.application.credentials.admin_email}>"
+  end
+
+  def status_updated(ticket_id, old_status)
+    @ticket = Support::Ticket.find(ticket_id)
+    @old_status = old_status
+    mail to: "#{@ticket.user.name} <#{@ticket.user.email}>",
+    from: 'PincannaRx Support <staff-support@pincanna.org>',
+    subject: "[#{@ticket.ticket_number}] Ticket Status Updated"
   end
 end
