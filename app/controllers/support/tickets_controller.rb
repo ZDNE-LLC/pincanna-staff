fedad12cfaeb4a537ee78823e0c7c35e0a9385e9
Zach Nusbaum <zachdnusbaum@gmail.com>
2018-03-06 23:51:50 -0800

    login fixes

--- app/controllers/support/tickets_controller.rb
+++ app/controllers/support/tickets_controller.rb
@@ -1,5 +1,5 @@
 class Support::TicketsController < ApplicationController
-  before_action :ensure_gsuite
+  before_action :authenticate_user!
 
   def index
     @tickets = current_user.support_tickets
